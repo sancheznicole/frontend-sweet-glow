c0fb67acc17e995a35873edae6db9ea42cc9b3bb
Luisa <luisajr1915@gmail.com>
2026-03-07 13:32:58 -0500

    Blog

--- src/pages/blog/BlogPage.jsx
+++ src/pages/blog/BlogPage.jsx
@@ -2,7 +2,18 @@ import React from 'react'
 
 const BlogPage = () => {
   return (
-    <div>BlogPage</div>
+    <div className="page-container">
+
+      <div className="Blog">
+        
+        <img className="img"
+          src="http://127.0.0.1:8000/storage/imagenes_productos/cuidadodelapiel.jpg" 
+          alt="Cuidado de la Piel" 
+        />
+        <a href="#" className="link">Skin Care</a>
+
+      </div>
+    </div>
   )
 }
 
