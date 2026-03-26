068e5b424af6a9f03311c399bbe20ca6de5e16de
Luisa <luisajr1915@gmail.com>
2026-03-26 13:49:14 -0500

    cambios

--- src/pages/blog/BlogPage.jsx
+++ src/pages/blog/BlogPage.jsx
@@ -1,50 +1,66 @@
 import React from "react"
-import { Link } from "react-router-dom";
+import { Link } from "react-router-dom"
 
 const BlogPage = () => {
   return (
-    <div className="blog-container">
-
-      <div className="Blog">
-        <img className="img"
-          src="http://127.0.0.1:8000/storage/imagenes_productos/skin.jpg" 
-          alt="Cuidado de la Piel" 
-        />
-        <Link to="/blog/Skincare">Cuidado de la piel</Link>
+    <div>
+      <div className="blog-header">
+        <h1>CONSEJOS Y TENDENCIAS DE BELLEZA</h1>
       </div>
 
-      <div className="Blog">
-        <img className="img"
-          src="http://127.0.0.1:8000/storage/imagenes_productos/tonosideales.jpg" 
-          alt="Tonos ideales" 
-        />
-        <Link to="/blog/Colorimetria">Colorimetria y tonos ideales</Link>
-      </div>
+      <div className="blog-container">
 
-      <div className="Blog">
-        <img className="img"
-          src="http://127.0.0.1:8000/storage/imagenes_productos/fragancia.jpg" 
-          alt="Fragancia" 
-        />
-        <Link to="/blog/Fragrance">Fragancias</Link>
-      </div>
+        <div className="Blog">
+          <img className="img"
+            src="http://127.0.0.1:8000/storage/imagenes_productos/skin.jpg"
+            alt="Cuidado de la Piel"
+          />
+          <div className="blog-overlay">
+            <Link to="/blog/Skincare">Cuidado de la piel</Link>
+          </div>
+        </div>
 
-      <div className="Blog">
-        <img className="img"
-          src="http://127.0.0.1:8000/storage/imagenes_productos/productos.jpg" 
-          alt="Productos" 
-        />
-        <Link to="/blog/Recommended">Productos recomendados</Link>
-      </div>
+        <div className="Blog">
+          <img className="img"
+            src="http://127.0.0.1:8000/storage/imagenes_productos/tonosideales.jpg"
+            alt="Tonos ideales"
+          />
+          <div className="blog-overlay">
+            <Link to="/blog/Colorimetria">Colorimetria y tonos ideales</Link>
+          </div>
+        </div>
 
-      <div className="Blog">
-        <img className="img"
-          src="http://127.0.0.1:8000/storage/imagenes_productos/belleza.jpg" 
-          alt="Consejos" 
-        />
-        <Link to="/blog/Advice">Consejos de belleza</Link>
-      </div>
+        <div className="Blog">
+          <img className="img"
+            src="http://127.0.0.1:8000/storage/imagenes_productos/fragancia.jpg"
+            alt="Fragancia"
+          />
+          <div className="blog-overlay">
+            <Link to="/blog/Fragrance">Fragancias</Link>
+          </div>
+        </div>
 
+        <div className="Blog">
+          <img className="img"
+            src="http://127.0.0.1:8000/storage/imagenes_productos/productos.jpg"
+            alt="Productos"
+          />
+          <div className="blog-overlay">
+            <Link to="/blog/Recommended">Productos recomendados</Link>
+          </div>
+        </div>
+
+        <div className="Blog">
+          <img className="img"
+            src="http://127.0.0.1:8000/storage/imagenes_productos/belleza.jpg"
+            alt="Consejos"
+          />
+          <div className="blog-overlay">
+            <Link to="/blog/Advice">Consejos de belleza</Link>
+          </div>
+        </div>
+
+      </div>
     </div>
   )
 }
